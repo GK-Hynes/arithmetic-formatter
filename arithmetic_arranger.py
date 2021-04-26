4bf4597275386c9b290fba11b8a8787045c827c3
Gerard Hynes <gkhynes@gmail.com>
2021-04-26 17:04:57 +0100

    Optionally print answers

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,16 +1,8 @@
-def arithmetic_arranger(problems):
-    # Input
-    # ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-
-    # Output
-    #    32      3801      45      123
-    # + 698    -    2    + 43    +  49
-    # -----    ------    ----    -----
+def arithmetic_arranger(problems, bool=False):
     first_rows = ""
     second_rows = ""
     all_lines = ""
-    # operators = ""
-    # answers = ""
+    answers = ""
     arranged_problems = ""
 
     for problem in problems:
@@ -18,7 +10,6 @@ def arithmetic_arranger(problems):
         first_row = p[0]
         second_row = p[-1]
         operator = p[1]
-        # print(f"First {first_row} - second {second_row} - operator {operator}")
 
         if len(first_row) > len(second_row):
             second_row = operator + " " * (len(first_row) - len(second_row)) + " " + second_row
@@ -32,17 +23,29 @@ def arithmetic_arranger(problems):
             second_row = operator + " " + second_row
             first_row = " " * 2 + first_row
             lines = "-" * len(second_row)
+
+        if bool == True:
+            answer = str(eval(problem))
+            answer = " " * (len(lines) - len(answer)) + answer
+        else:
+            answer = ""
             
         first_rows = first_rows + first_row + "   "
         second_rows = second_rows + second_row + "   "
         all_lines = all_lines + lines + "   "
+        answers = answers + answer + "   "
     
-    print(first_rows)
-    print(second_rows)
-    print(all_lines)
-    arranged_problems = first_rows + "\n" + second_rows + "\n" + all_lines + "\n"
-    # print(arranged_problems)
+    # print(first_rows)
+    # print(second_rows)
+    # print(all_lines)
+
+    if bool == True:
+        arranged_problems = first_rows + "\n" + second_rows + "\n" + all_lines + "\n" + answers
+    else:
+        arranged_problems = first_rows + "\n" + second_rows + "\n" + all_lines + "\n"
+    print(arranged_problems)
         # return arranged_problems
 
-# arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"])
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
