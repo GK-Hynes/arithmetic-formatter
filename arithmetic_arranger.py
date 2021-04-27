579e09c610b349dd2b473ca56ac2de1750a97349
Gerard Hynes <gkhynes@gmail.com>
2021-04-27 13:17:34 +0100

    Check for invalid inputs

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,12 +5,30 @@ def arithmetic_arranger(problems, bool=False):
     answers = ""
     arranged_problems = ""
 
-    for problem in problems:
-        p = problem.split(" ")
-        first_row = p[0]
-        second_row = p[-1]
-        operator = p[1]
+    # Limit problems to 5
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+    for problem in problems:
+        # Generate rows
+        parts = problem.split(" ")
+        first_row = parts[0]
+        second_row = parts[-1]
+        operator = parts[1]
+
+        # Make sure operator is + or -
+        if operator not in "+-":
+            return "Error: Operator must be '+' or '-'."
+        
+        # Make sure operands only contain digits
+        if not first_row.isdecimal() or not second_row.isdecimal():
+            return "Error: Numbers must only contain digits."
+
+        # Make sure numbers are no more than 4 digits
+        if len(first_row) > 4 or len(second_row) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        # Start formatting
         if len(first_row) > len(second_row):
             second_row = operator + " " * (len(first_row) - len(second_row)) + " " + second_row
             lines = "-" * (len(first_row) + 2)
@@ -24,28 +42,27 @@ def arithmetic_arranger(problems, bool=False):
             first_row = " " * 2 + first_row
             lines = "-" * len(second_row)
 
+        # Check whether to generate answers
         if bool == True:
             answer = str(eval(problem))
             answer = " " * (len(lines) - len(answer)) + answer
         else:
             answer = ""
-            
-        first_rows = first_rows + first_row + "   "
-        second_rows = second_rows + second_row + "   "
-        all_lines = all_lines + lines + "   "
-        answers = answers + answer + "   "
-    
-    # print(first_rows)
-    # print(second_rows)
-    # print(all_lines)
+        
+        # Assemble formatted rows
+        first_rows = first_rows + first_row + "    "
+        second_rows = second_rows + second_row + "    "
+        all_lines = all_lines + lines + "    "
+        answers = answers + answer + "    "
 
+    # Check whether to return problem answers too
     if bool == True:
         arranged_problems = first_rows + "\n" + second_rows + "\n" + all_lines + "\n" + answers
     else:
         arranged_problems = first_rows + "\n" + second_rows + "\n" + all_lines + "\n"
     print(arranged_problems)
-        # return arranged_problems
+    # return arranged_problems
 
 
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+arithmetic_arranger(["32 + 68", "301 - 2", "45 + 43", "123 + 49"])
 arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
